3fcd8f98fc809f64880d65a666881f28dbd31c79
Umar <umarattique637@gmail.com>
2025-08-26 17:16:37 +0500

    section 3

---
+++ section_03/i.js
@@ -0,0 +1,47 @@
+
+// let obj = {
+//   "fname" : "Umar",
+//   "lname" : "Attique"
+// }
+
+// function fun(o)
+// {
+//   let x = `Your name is ${o.fname} " " ${o.lname}`
+//   return  x
+// }
+
+
+
+
+// let cal = fun(obj)
+// console.log(cal)
+
+// let obj = {
+//   fname: "Umar",
+//   lname: "Attique",
+//   myfun: function fun() {
+//     let x = `Your name is ${this.fname} " " ${this.lname}`;
+//     return x;
+//   },
+// };
+
+// let cal = obj.myfun()
+// console.log(cal);
+
+
+// let x = (a,b) => 
+// {
+//   let z = a+b
+//   return z
+// }
+// console.log(x(4,4))
+
+
+
+// let x = (a,b) =>  (a+b , {user: 'umar' , 'sum' : a+b})
+// console.log(x(4,4))
+
+(function fun()
+{
+  console.log("DB Connected")
+})()
